2227ade61a10a4560b908e885a30d37a4ced2341
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-21 22:50:09 -0500

    Creada la estructura del menú principal

--- registro.py
+++ registro.py
@@ -1,6 +1,7 @@
+import re
 def mostrarMenu(): #Función para mostrar el menú principal
     while(True):
-        print(22*"#"+" MENÚ PRINCIPAL "+"#"*22)
+        print(22*"*"+" MENÚ PRINCIPAL "+"*"*22)
         print("[1] Cargar información de CSV")
         print("[2] Registrar participantes")
         print("[3] Buscar participante")
@@ -10,7 +11,30 @@ def mostrarMenu(): #Función para mostrar el menú principal
         print("[7] Actualizar información de CSV")
         print("[8] Serializar información a JSON")
         print("[0] Salir")
-        toDo = int(input('¿Qué opción deseas?: '))
+        toDo = input('¿Qué opción deseas?: ')
+        if bool(re.match('^[0-9]{1}$', toDo)):
+            if toDo == "1":
+                pass
+            elif toDo == "2":
+                pass
+            elif toDo == "3":
+                pass
+            elif toDo == "4":
+                pass
+            elif toDo == "5":
+                pass
+            elif toDo == "6":
+                pass
+            elif toDo == "7":
+                pass
+            elif toDo == "8":
+                pass
+            elif toDo == "0":
+                pass
+        else:
+            print("*"*60)
+            print('Opción inválida...')
+            input("Presiona ENTER para continuar...")
         break
         
 def main(): #Función principal
