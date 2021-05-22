1db146390d116e9ddc2ecabb4345c859c5635cc6
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-21 22:20:21 -0500

    Modificación del metodo mostrarInfo

--- clases.py
+++ clases.py
@@ -2,15 +2,14 @@ from datetime import datetime #Importamos datetime para agregar el momento autom
 
 class Participante: #Creación de la clase
     def __init__(self, correo, nombre, nacimiento, momento = datetime.now()):
-        self.__correo = correo
-        self.__nombre = nombre
-        self.__nacimiento = nacimiento
-        self.__momento = momento
+        self._correo = correo
+        self._nombre = nombre
+        self._nacimiento = nacimiento
+        self._momento = momento.strftime("%Y-%M-%D %H:%M")
     
-    @classmethod
     def mostrarInfo(self): #Metodo para mostrar la información del participante
-        correo = self.__correo
-        nombre = self.__nombre
-        nacimiento = self.__nacimiento
-        momento = self.__momento
+        correo = self._correo
+        nombre = self._nombre
+        nacimiento = self._nacimiento
+        momento = self._momento
         return(f'Correo: {correo}, Nombre: {nombre}, Nacimiento: {nacimiento}, Momento: {momento}')
